0b5a061688fdb035dd3bccee4124a4c5a2440dcd
Армен Алумян <alumyanas@altarix.ru>
2018-11-09 22:46:02 +0400

    Initial Commit

---
+++ Chord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chord
+//
+//  Created by Армен Алумян on 09/11/2018.
+//  Copyright © 2018 Армен Алумян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
